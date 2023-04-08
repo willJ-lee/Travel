eae594f27a84ce4e32f13819e69a2bc15499f98e
liweijie <liweijiechn@gmail.com>
2023-04-08 14:31:49 +0800

    full code

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      请输入城市/景点/游玩主题
+    </div>
+    <router-link to="/city">
+      <div class="header-right">{{this.city}}
+        <span class="iconfont arrow-icon">&#xe6aa;</span>
+      </div>
+    </router-link>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'HomeHeader',
+  computed: {
+    ...mapState(['city'])
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .header
+    background: $bgColor
+    display: flex
+    line-height: $headerHeight
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left .2rem
+      padding-left: .2rem
+      background: #fff
+      flex: 1
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      min-width: 1.04rem
+      padding: 0 .1rem
+      float: right
+      text-align: center
+      color: #fff
+      .arrow-icon
+        font-size: .24rem
+        margin-left: -.04rem
+</style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <router-link tag="li" :to="'/detail/' + item.id" class="item border-bottom" v-for="item of list" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    height: 1.9rem
+    display: flex
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      min-width: 0
+      flex: 1
+      padding: .1rem
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        height: .4rem
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        margin-top: .2rem
+        background: #ff9300
+        padding: 0 .06rem
+        color: #fff
+</style>
